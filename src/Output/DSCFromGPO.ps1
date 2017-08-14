313802784149a5253283aac865af663842ad425a
Paul DeArment <paul.dearmentjr@aurora.org>
2017-08-14 15:45:27 -0500

    updates to test adding multiple gpos

---
+++ src/Output/DSCFromGPO.ps1
@@ -0,0 +1,19 @@
+﻿       
+Configuration DSCFromGPO
+{
+
+	Import-DSCResource -ModuleName 'PSDesiredStateConfiguration'
+	Import-DSCResource -ModuleName 'AuditPolicyDSC'
+	Import-DSCResource -ModuleName 'SecurityPolicyDSC'
+	Import-DSCResource -ModuleName 'BaselineManagement'
+	Import-DSCResource -ModuleName 'xSMBShare'
+	Import-DSCResource -ModuleName 'DSCR_PowerPlan'
+	Import-DSCResource -ModuleName 'xScheduledTask'
+	Import-DSCResource -ModuleName 'Carbon'
+	# Module Not Found: Import-DSCResource -ModuleName 'PrinterManagement'
+	# Module Not Found: Import-DSCResource -ModuleName 'rsInternationalSettings'
+	Node localhost
+	{
+	}
+}
+DSCFromGPO -OutputPath 'C:\Users\949237\Documents\Repos\BaselineManagement\src\Output'
